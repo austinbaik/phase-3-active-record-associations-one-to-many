8e8c3de7cf648f9b57647e5a30b3475c6a6af31b
Austin <baik.austin@gmail.com>
2022-12-11 18:16:34 -0800

    commit tree 4c9b6c826be2bebf407e6247a615df0f6756791c

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,14 @@
 class Review < ActiveRecord::Base
+
+    belongs_to(:game)
+    #or 
+    #belongs_to :game
+    #MUST BE SINGULAR
+
+
+    # def game
+    #     #self is the review instance
+    #     Game.find(self.game_id)
+    # end
   
 end
